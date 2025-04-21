0fa83f9a749e470480c12d2d9a5f8e6076c9d85f
bNunley07 <benjamin.nunley@stu.daviess.kyschools.us>
2025-04-21 03:08:58 +0000

    ddddd

--- AInsertionSort.java
+++ AInsertionSort.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class AInsertionSort
 {
 
@@ -5,5 +7,21 @@ public class AInsertionSort
     {
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
 
+        int copy = 0;
+        int loc = 0;
+        for(int i = 1; i < myNumbers.length;i++){
+            loc = i;
+
+            while(myNumbers[loc] < myNumbers[loc-1] && loc>=0)
+            {
+                copy = myNumbers[loc-1];
+                myNumbers[loc-1] = myNumbers[loc];
+                myNumbers[loc] = copy;
+                loc--;
+            }
+
+	    
+        }
+        System.out.println(Arrays.toString(myNumbers));
     }
 }
--- ALInsertionSort.java
+++ ALInsertionSort.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+ import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ALInsertionSort
 {
@@ -8,26 +9,35 @@ public class ALInsertionSort
     	Integer myNumbers[] = {0, 15, 6, 8, 2, 37};
     	ArrayList<Integer> sorted = new ArrayList<Integer>();
     	boolean inserted=false;
-
+		int loc = 0;
 		//Add the first number to the array list
-
+		sorted.add(myNumbers[0]);
 		//Loop once for all of the remaining numbers in the unsorted list
+		for(int i =1; i< myNumbers.length;i++)
+		{
+			inserted = false;
+			loc = 0;
 
-			//Check with each of the numbers in the sorted list
-
-				//If this number is less than one in the sorted list,
-				//insert it there
-
-			//If it was not inserted, stick it on the end.
-
-
-		//End Loop for unsorted list
-
-		System.out.println(sorted);
-
-		//Move the data back to the array
-
-		//Print the contents of the array
+			while(inserted == false && loc < sorted.size())
+			{
+				if(myNumbers[i] <sorted.get(loc))
+				{
+					sorted.add(loc, myNumbers[i]);
+					inserted =true;
+				}
+				loc++;
+			}
+		if(!inserted)
+		{
+			sorted.add(myNumbers[i]);
+		}
+	
+		}
+		for(int c =1; c< myNumbers.length;c++)
+		{
+			myNumbers[c] = sorted.get(c);
+		}
+		System.out.println(Arrays.toString(myNumbers));
 
     }
 
